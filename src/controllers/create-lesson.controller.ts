307b9e00e233cfa26249a978e465b5557f576882
Yuri Palacio <yurifpalacio@gmail.com>
2024-08-30 13:23:25 -0300

    Finishing CreateLessonController

--- src/controllers/create-lesson.controller.ts
+++ src/controllers/create-lesson.controller.ts
@@ -1,16 +1,51 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createLessonBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateLessonBodySchema = z.infer<typeof createLessonBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createLessonBodySchema)
 
 @Controller('/lessons')
 @UseGuards(JwtAuthGuard)
 export class CreateLessonController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
-    return 'ok'
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(bodyValidationPipe) body: CreateLessonBodySchema,
+  ) {
+    const { title, content } = body
+    const { sub: userId } = user
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.lesson.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
   }
 }
