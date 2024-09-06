5f3930fe5030d2f51c4158939df618067ab2d66f
Yuri Palacio <yurifpalacio@gmail.com>
2024-09-06 14:51:31 -0300

    Implementing Prisma Repositories

--- src/infra/database/prisma/repositories/prisma-enrolls-repository.ts
+++ src/infra/database/prisma/repositories/prisma-enrolls-repository.ts
@@ -2,25 +2,60 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { EnrollsRepository } from '@/domain/event/application/repositories/enrolls-repository'
 import { Enroll } from '@/domain/event/enterprise/entities/enroll'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaEnrollMapper } from '../mappers/prisma-enroll-mapper'
 
 @Injectable()
 export class PrismaEnrollsRepository implements EnrollsRepository {
+  constructor(private prisma: PrismaService) {}
+
   async findById(id: string): Promise<Enroll | null> {
-    throw new Error('Method not implemented.')
+    const enroll = await this.prisma.enroll.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!enroll) {
+      return null
+    }
+
+    return PrismaEnrollMapper.toDomain(enroll)
   }
 
   async findManyByLessonId(
     lessonId: string,
-    params: PaginationParams,
+    { page }: PaginationParams,
   ): Promise<Enroll[]> {
-    throw new Error('Method not implemented.')
+    const enrolls = await this.prisma.enroll.findMany({
+      where: {
+        lessonId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return enrolls.map(PrismaEnrollMapper.toDomain)
   }
 
   async create(enroll: Enroll): Promise<void> {
-    throw new Error('Method not implemented.')
+    const data = PrismaEnrollMapper.toPrisma(enroll)
+
+    await this.prisma.enroll.create({
+      data,
+    })
   }
 
   async delete(enroll: Enroll): Promise<void> {
-    throw new Error('Method not implemented.')
+    const data = PrismaEnrollMapper.toPrisma(enroll)
+
+    await this.prisma.enroll.delete({
+      where: {
+        id: data.id,
+      },
+    })
   }
 }
--- src/infra/database/prisma/repositories/prisma-lesson-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-lesson-attachments-repository.ts
@@ -1,16 +1,30 @@
 import { LessonAttachmentsRepository } from '@/domain/event/application/repositories/lesson-attachments-repository'
 import { LessonAttachment } from '@/domain/event/enterprise/entities/lesson-attachment'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaLessonAttachmentMapper } from '../mappers/prisma-lesson-attachment-mapper'
 
 @Injectable()
 export class PrismaLessonAttachmentsRepository
   implements LessonAttachmentsRepository
 {
+  constructor(private prisma: PrismaService) {}
+
   async findManyByLessonId(lessonId: string): Promise<LessonAttachment[]> {
-    throw new Error('Method not implemented.')
+    const lessonAttachments = await this.prisma.attachment.findMany({
+      where: {
+        lessonId,
+      },
+    })
+
+    return lessonAttachments.map(PrismaLessonAttachmentMapper.toDomain)
   }
 
   async deleteManyByLessonId(lessonId: string): Promise<void> {
-    throw new Error('Method not implemented.')
+    await this.prisma.attachment.deleteMany({
+      where: {
+        lessonId,
+      },
+    })
   }
 }
